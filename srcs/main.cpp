97d23b37a3089c6b2bbe955f91f7bafbc3702a6c
Nicolas Keita <nicolas.keita@epitech.eu>
2019-12-29 17:02:41 +0100

    USer is able to configure the target serverAddress through program arguments

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,12 +2,35 @@
 #include "GraphicWrapper.hpp"
 #include "NetworkHandler.hpp"
 #include "Macro.hpp"
+#include "MyProgArgs.hpp"
 
-int main(int argc, char *argv[])
+void startVoiceCommunication(babel::NetworkHandler &network,
+                             const uti::MyProgArgs &args)
 {
+    unsigned int    port = 42424;
+    std::string     serverAddress = "0.0.0.0";
+
+    if (args.getSize() > 2) {
+        try {
+            port = std::stoi(args.getArgs().at(2));
+            if (port < 2000)
+                throw std::invalid_argument("Pick higher port");
+            serverAddress = args.getArgs().at(1);
+        }
+        catch (std::invalid_argument &e) {
+            std::cerr << "Wrong port\n" << e.what() << std::endl;
+            exit(84);
+        }
+    }
+    network.startVoiceCommunication(serverAddress, port);
+}
+
+int main(int argc, char *argv[], char **env)
+{
+    uti::MyProgArgs args(argc, argv, env, 0);
     babel::NetworkHandler   network;
-    network.startVoiceCommunication("0.0.0.0",
-                                    std::stoi(argv[1]));
+
+    startVoiceCommunication(network, args);
 
     // Graphic
     QApplication                    app(argc, argv);
